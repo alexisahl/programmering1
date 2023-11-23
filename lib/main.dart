65b14012638f7d1605c7aed3ff9ce37943b6817a
alexisahl <meltycsgo@gmail.com>
2023-11-23 11:32:24 +0200

    ny försök

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override 
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "Hello World Demo App",
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.red
+        )
+      ),
+      home: const MyHomePage(title: "Home Page"),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class MyHomePage extends StatelessWidget{
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title)
+    ),
+    body: const Center(
+      child: Text(
+        'Welcome to Flytter'
+      )),
+    );
+  }
+}
