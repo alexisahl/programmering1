a8b983d4623fa533f0f71f1dc03b01224c7f7a3f
alexisahl <meltycsgo@gmail.com>
2023-11-27 10:30:35 +0200

    Knapp update

--- lib/test.dart
+++ lib/test.dart
@@ -10,15 +10,22 @@ class Home extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My third app"),
+        title: const Text("My third app", style: TextStyle(color: Colors.yellow)),
         centerTitle: true,
         backgroundColor: Colors.red[200],
       ),
       body: const Center(
       child: Text("We are doing this again",
-      style: TextStyle(fontSize: 20.0,),
+      style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 0, 20)),
       ),
       ),
+      floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.red,
+      onPressed: () =>{
+        SystemNavigator.pop()
+      }, 
+      child: const Text('Exit', style: TextStyle(color: Colors.black))
+    ),
     );
   }
 }
